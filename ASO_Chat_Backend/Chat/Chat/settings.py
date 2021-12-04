920fa6c8c928ea0e36612bc0e4dd54220a4dfb6a
DDD9 <dumitrescu549@gmail.com>
2021-12-05 00:20:30 +0200

    Transition from SQLite to PSQL

--- ASO_Chat_Backend/Chat/Chat/settings.py
+++ ASO_Chat_Backend/Chat/Chat/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Chat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_aso',
+        'USER': 'postgres',
+        'PASSWORD': 'Deedee112',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
